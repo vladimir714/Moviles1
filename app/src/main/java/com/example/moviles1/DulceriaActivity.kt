ad23f9d3a24a35407ab96b778d902a9cec368937
Reyleig <50756544+Reyleig@users.noreply.github.com>
2020-05-11 18:38:31 -0500

    comit

---
+++ app/src/main/java/com/example/moviles1/DulceriaActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moviles1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviles1.R
+
+class DulceriaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dulceria)
+    }
+}
