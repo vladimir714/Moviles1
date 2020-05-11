b98c9ba8a1cd9a19c4ba9c0d479c075d0c1f8516
Erik S <erik.steven17@gmail.com>
2020-05-11 18:03:33 -0500

    -- Se versionan cambios en dulceria

---
+++ app/src/main/java/com/example/moviles1/reserva_dulceria_fragment.kt
@@ -0,0 +1,12 @@
+package com.example.moviles1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reserva_dulceria_fragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserva_dulceria_fragment)
+    }
+}
